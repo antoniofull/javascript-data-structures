663d2b17513b3d96bce2d75a71a5a9d80f0a57a7
Antonio <antonio.fullone@gmail.com>
2019-04-08 11:07:41 +0200

    Singly Linked List added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # javascript-data-structures
+
 Classic CS Data Structures with Javascript
+
+## Singly Linked List
---
+++ main.js
@@ -0,0 +1,9 @@
+import SinglyLinkedList from './src/SinglyLinkedList.js';
+const List = new SinglyLinkedList();
+List.push(1);
+List.push(2);
+List.push(3);
+List.push(4);
+List.push(5);
+
+window.List = List;
